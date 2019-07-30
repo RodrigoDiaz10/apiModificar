3cc2e181f91455a21b171886a89f000585016d80
RodrigoDiaz10 <raa.diaz@yavirac.edu.ec>
2019-07-29 20:08:39 -0500

    ingreso del resto de tablas

--- apiBooks/server/create-lb-tables.js
+++ apiBooks/server/create-lb-tables.js
@@ -1,6 +1,7 @@
 var server = require('./server');
     var ds = server.dataSources.db;
-    var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role','book'];//poner todas las tablas de modelos
+    var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role','book','form',
+    'proyect','qualification','student','teacher'];                               //poner todas las tablas de modelos
     ds.automigrate(lbTables, function(er) {//                                     entrar a la carpeta server y ejecutar node create-lb-tables.js
         if (er) throw er;
         console.log('Loopback tables [' - lbTables - '] created in ', ds.adapter.name);
